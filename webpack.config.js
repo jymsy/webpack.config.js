8d30d8e566b809deb00969b0f30f520acc300642
jymsy <jym20083617@163.com>
2017-04-11 08:26:34 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 var webpack = require('webpack');
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var failPlugin = require('webpack-fail-plugin');
 var path = require('path');
 
 module.exports = {
     entry: { //配置入口文件，有几个写几个
         lc: './js/src/modules/learning_center/index.js',
         ls: './js/src/modules/learning_system/index.js',
+        tc: './js/src/modules/teaching_center/index.js',
         review: './js/src/modules/review/index.js',
         purchased: './js/src/modules/purchased/purchased.js',
         p_detail: './js/src/modules/count_analyze/count_analyze.js',
-        header: './js/src/public/header.js',
         oraltest: './js/src/modules/oraltest/index.js',
+        upgrade: './js/src/modules/upgrade/upgrade.js',
+        accounting: './js/src/modules/upgrade/accounting.js',
+        // vendor: ['./js/src/lib/layer/layer.js']
         vender: ['jquery', 'lodash']
     },
     output: {
         path: 'js/dist', //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
         publicPath: '/static/js/dist/', //模板、样式、脚本、图片等资源对应的server上的路径
-        filename: '[name].js',            //每个页面对应的主js的生成配置
-        chunkFilename: '[id].chunk.js',   //chunk生成的配置
-        // sourceMapFilename: '[name].js.map'
+        filename: '[name].js?v=[chunkhash:8]',            //每个页面对应的主js的生成配置
+        chunkFilename: '[name].js'   //chunk生成的配置
     },
-    // devtool: 'source-map',
-    // devtool: 'cheap-module-eval-source-map',
+    // devtool: 'cheap-module-source-map',
     module: {
         preLoaders: [
-            // {
-            //     test: /\.js$/,
-            //     loader: "eslint-loader",
-            //     exclude: /node_modules/
-            // },
+            {
+                test: /\.js$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/
+            },
             // {
             //     test: /\.js$/,
             //     loader: "source-map-loader",
@@ -47,13 +50,27 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+            },
+            {
+                test: /\.(jpg|png|jpeg|gif)$/,
+                loader: 'url?name=images/[name].[ext]&limit=8192'
+                // loader: "file-loader?name=[name].[ext]&publicPath=assets/foo/&outputPath=app/images/"
             }
-
         ]
     },
+    watchOptions: {
+        poll: 1000
+    },
+    resolve: {
+        alias: {
+            public: path.resolve(__dirname, './js/src/public'),
+            // layer: path.resolve(__dirname, './js/src/lib/layer/layer.js'),
+            layerDir: path.resolve(__dirname, './js/src/lib/layer')
+        }
+    },
     eslint: {
-        failOnWarning: true,
+        failOnWarning: false,
         failOnError: true,
         configFile: './.eslintrc'
     },
@@ -63,57 +80,73 @@ module.exports = {
         async: 'async',
         echarts: 'echarts',
         cyberplayer: 'cyberplayer',
-        clipboard: 'Clipboard'
+        clipboard: 'Clipboard',
+        laydate: 'laydate'
+        // layer: 'layer'
     },
     plugins: [
+        failPlugin,
         new webpack.optimize.CommonsChunkPlugin({
-            // name: 'common', // 将公共模块提取，生成名为`vendors`的chunk
-            names: ["common", "vender"],
-            // chunks: ['lc','ls','review', 'purchased', 'p_detail','oraltest','vender'], //提取哪些模块共有的部分
+            // names: ['common', 'vendor'],
+            // name: 'common', // 将公共模块提取
+            names: ['common', 'vender'],
+            // chunks: ['lc','ls','tc','review', 'purchased', 'p_detail','oraltest'], //提取哪些模块共有的部分
             minChunks: 4 // 提取至少n个模块共有的部分
-            // minChunks: Infinity // 提取至少n个模块共有的部分
         }),
+        new ExtractTextPlugin('css/[name].css?v=[chunkhash:8]'),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_system/views/main.php'),
-            template: 'views/main.php',
-            chunks: ['header', 'common', 'ls'],
+            template: 'views/main_blue.php',
+            chunks: ['common', 'ls'],
             inject:'body',
-            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_center/views/main.php'),
-            template: 'views/main.php',
-            chunks: ['header', 'common', 'lc'],
+            template: 'views/main_white.php',
+            chunks: ['common', 'lc'],
             inject:'body',
-            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_center/views/oral_main.php'),
-            template: 'views/main.php',
-            chunks: ['header', 'common', 'oraltest'],
+            template: 'views/main_white.php',
+            chunks: ['common', 'oraltest'],
             inject:'body',
-            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_center/views/purchased_main.php'),
-            template: 'views/main.php',
-            chunks: ['header', 'common', 'purchased'],
+            template: 'views/main_white.php',
+            chunks: ['common', 'purchased'],
             inject:'body',
-            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_center/views/purchased_detail_main.php'),
-            template: 'views/main.php',
-            chunks: ['header', 'common', 'p_detail'],
+            template: 'views/main_white.php',
+            chunks: ['common', 'p_detail'],
             inject:'body',
-            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, '../application/modules/learning_system/views/review_page.php'),
             template: 'views/review_main.php',
-            chunks: ['vender', 'common', 'review'],
+            chunks: ['common', 'review'],
             inject:'body',
-            hash: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, '../application/modules/teaching_center/views/main.php'),
+            template: 'views/main_white.php',
+            chunks: ['common', 'tc'],
+            inject:'body',
+        }),
+        new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, '../application/modules/upgrade/views/upgrade.php'),
+            template: 'views/upgrade_main.php',
+            chunks: ['upgrade'],
+            inject:'body',
+        }),
+        new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, '../application/modules/upgrade/views/accounting.php'),
+            template: 'views/upgrade_main.php',
+            chunks: ['accounting', 'vendor'],
+            inject:'body',
         }),
         // new uglifyJsPlugin({
         //     compress: {
@@ -122,7 +155,7 @@ module.exports = {
         //     output: {
         //         comments: false
         //     },
-        //     sourceMap: true
+        //     except: ['$', 'exports', 'require']
         // })
     ]
 };
